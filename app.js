20acbb26f8721620a121a4b795c0074f8ab1b828
RohiV04 <rohith_csd@srkrec.edu.in>
2023-10-30 11:05:45 +0530

    test-1

--- app.js
+++ app.js
@@ -2,82 +2,86 @@ const express = require("express");
 const TelegramBot = require("node-telegram-bot-api");
 
 const app = express();
+const translate = require("./translate");
 // replace the value below with the Telegram token you receive from @BotFather
 const token = "6729574087:AAEfsxp8trqnN5VOBOJcvTm-7nJhyxO64Lo";
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of
-// messages.
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
-
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, "Hello");
-// });
-
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Welcome");
 });
 
-// Keyboard Functionality
-bot.onText(/\/help/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome", {
-    reply_markup: {
-      keyboard: [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]],
-    },
-  });
-});
 
-const language = [
-  ["English", "Hindi"],
-  ["Telugu", "Tamil"],
-  ["Kannada", "Malayalam"],
-];
-//translate one language to another the msg given by the user
-bot.onText(/\/translate/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome", {
-    reply_markup: {
-      keyboard: language,
-    },
-  });
-});
+app.use(translate(bot));
+// // Matches "/echo [whatever]"
+// bot.onText(/\/echo (.+)/, (msg, match) => {
+//   // 'msg' is the received Message from Telegram
+//   // 'match' is the result of executing the regexp above on the text content
+//   // of the message
 
-//send a photo
-bot.onText(/\/pic/, (msg) => {
-  bot.sendPhoto(msg.chat.id, "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg");
-});
+//   const chatId = msg.chat.id;
+//   const resp = match[1]; // the captured "whatever"
 
-//send a document
-bot.onText(/\/doc/, (msg) => {
-  bot.sendDocument(msg.chat.id, "./../../../Documents/Certificates/Coursera GenAi.pdf");
-});
+//   // send back the matched "whatever" to the chat
+//   bot.sendMessage(chatId, resp);
+// });
 
-//send a audio
-bot.onText(/\/audio/, (msg) => {
-  bot.sendAudio(msg.chat.id, "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg");
-});
+// // Listen for any kind of message. There are different kinds of
+// // messages.
+// // bot.on("message", (msg) => {
+// //   const chatId = msg.chat.id;
 
-//send a video
-bot.onText(/\/video/, (msg) => {
-    bot.sendVideo(msg.chat.id, "./../../../Downloads/Telegram Desktop/001 - The Day I Became a Shinigami.mkv", {contentType: 'video/mp4'});
-});
+// //   // send a message to the chat acknowledging receipt of their message
+// //   bot.sendMessage(chatId, "Hello");
+// // });
 
-const PORT=process.env.PORT || 9000;
+
+
+// // Keyboard Functionality
+// bot.onText(/\/help/, (msg) => {
+//   bot.sendMessage(msg.chat.id, "Welcome", {
+//     reply_markup: {
+//       keyboard: [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]],
+//     },
+//   });
+// });
+
+// //send a photo
+// bot.onText(/\/pic/, (msg) => {
+//   bot.sendPhoto(
+//     msg.chat.id,
+//     "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg"
+//   );
+// });
+
+// //send a document
+// bot.onText(/\/doc/, (msg) => {
+//   bot.sendDocument(
+//     msg.chat.id,
+//     "./../../../Documents/Certificates/Coursera GenAi.pdf"
+//   );
+// });
+
+// //send a audio
+// bot.onText(/\/audio/, (msg) => {
+//   bot.sendAudio(
+//     msg.chat.id,
+//     "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg"
+//   );
+// });
+
+// //send a video
+// bot.onText(/\/video/, (msg) => {
+//   bot.sendVideo(
+//     msg.chat.id,
+//     "./../../../Downloads/Telegram Desktop/001 - The Day I Became a Shinigami.mkv",
+//     { contentType: "video/mp4" }
+//   );
+// });
+
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
