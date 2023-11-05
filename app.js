9af404ed923156d8048e48f0e0b3771a3229aa11
RohiV04 <rohith_csd@srkrec.edu.in>
2023-11-05 12:58:17 +0530

    Built1 Translator

--- app.js
+++ app.js
@@ -22,73 +22,13 @@ bot.onText(/\/start/, (msg) => {
 });
 
 translate(bot);
-// // Matches "/echo [whatever]"
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
-
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
-
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
-
-// // Listen for any kind of message. There are different kinds of
-// // messages.
-// // bot.on("message", (msg) => {
-// //   const chatId = msg.chat.id;
-
-// //   // send a message to the chat acknowledging receipt of their message
-// //   bot.sendMessage(chatId, "Hello");
-// // });
-
-
-
-// // Keyboard Functionality
-// bot.onText(/\/help/, (msg) => {
-//   bot.sendMessage(msg.chat.id, "Welcome", {
-//     reply_markup: {
-//       keyboard: [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]],
-//     },
-//   });
-// });
-
-// //send a photo
-// bot.onText(/\/pic/, (msg) => {
-//   bot.sendPhoto(
-//     msg.chat.id,
-//     "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg"
-//   );
-// });
-
-// //send a document
-// bot.onText(/\/doc/, (msg) => {
-//   bot.sendDocument(
-//     msg.chat.id,
-//     "./../../../Documents/Certificates/Coursera GenAi.pdf"
-//   );
-// });
-
-// //send a audio
-// bot.onText(/\/audio/, (msg) => {
-//   bot.sendAudio(
-//     msg.chat.id,
-//     "https://www.7movierulz.so/poster/tiger-nageswara-rao-2023-dvdscr-telugu-full-movie-watch-online-free.jpg"
-//   );
-// });
-
-// //send a video
-// bot.onText(/\/video/, (msg) => {
-//   bot.sendVideo(
-//     msg.chat.id,
-//     "./../../../Downloads/Telegram Desktop/001 - The Day I Became a Shinigami.mkv",
-//     { contentType: "video/mp4" }
-//   );
-// });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
 
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
--- translate.js
+++ translate.js
@@ -1,14 +1,13 @@
 const axios = require("axios");
-const e = require("express");
-const translate = (bot) => {
-  let languageOptions = [
-    ["English", "Spanish", "French", "German"],
-    ["Italian", "Russian", "Japanese", "Chinese"],
-    ["Korean", "Arabic", "Hindi", "Turkish"],
-    ["Malayalam", "Tamil", "Telugu", "Kannada"],
-    ["Marathi", "Gujarati", "Bengali", "Urdu"],
-    ["Punjabi", "Nepali"],
-  ];
+
+const languageOptions = [
+  ["English", "Spanish", "French", "German"],
+  ["Italian", "Russian", "Japanese", "Chinese"],
+  ["Korean", "Arabic", "Hindi", "Turkish"],
+  ["Malayalam", "Tamil", "Telugu", "Kannada"],
+  ["Marathi", "Gujarati", "Bengali", "Urdu"],
+  ["Punjabi", "Nepali"],
+];
 
   // Dictionary to map language options to their corresponding codes
   const languageCodes = {
@@ -60,57 +59,58 @@ const translate = (bot) => {
     // Catalan: "ca",
   };
 
-  bot.onText(/\/translate/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Select the language", {
-      reply_markup: {
-        keyboard: languageOptions,
-      },
-    });
+const sendLanguageSelectionMessage = (bot, chatId) => {
+  bot.sendMessage(chatId, "Select the language", {
+    reply_markup: {
+      keyboard: languageOptions,
+    },
   });
-  // Listen for incoming messages
+};
+
+const translateText = async (bot, chatId, languageId, text) => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.set("source_language", "en");
+  encodedParams.set("target_language", languageId);
+  encodedParams.set("text", text);
+
+  const options = {
+    method: "POST",
+    url: "https://text-translator2.p.rapidapi.com/translate",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "X-RapidAPI-Key": "e4d4507831msh8daf27e079eeb5cp15df2ejsn008380f9d487",
+      "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
+    },
+    data: encodedParams,
+  };
+
+  try {
+    const response = await axios.request(options);
+    const translatedText = response.data.data.translatedText;
+    bot.sendMessage(chatId, `Translated Text: ${translatedText}`);
+  } catch (error) {
+    console.error(error);
+    bot.sendMessage(chatId, "Error translating text");
+  }
+};
+
+const translate = (bot) => {
+  bot.onText(/\/translate/, (msg) => {
+    setTimeout(() => sendLanguageSelectionMessage(bot, msg.chat.id), 1000);
+  });
+
   bot.on("message", (msg) => {
     const chatId = msg.chat.id;
 
-    // Check if the message is a language selection
-    if (msg.text in languageCodes === true) {
+    if (msg.text in languageCodes) {
       bot.sendMessage(chatId, "Enter the text to translate");
       const languageId = languageCodes[msg.text];
-      // Translate the text
-      bot.once("message", async (msg) => {
-        const chatId = msg.chat.id;
-        const encodedParams = new URLSearchParams();
-        encodedParams.set("source_language", "en");
-        encodedParams.set("target_language", languageId);
-        encodedParams.set("text", msg.text);
 
-        const options = {
-          method: "POST",
-          url: "https://text-translator2.p.rapidapi.com/translate",
-          headers: {
-            "content-type": "application/x-www-form-urlencoded",
-            "X-RapidAPI-Key":
-              "e4d4507831msh8daf27e079eeb5cp15df2ejsn008380f9d487",
-            "X-RapidAPI-Host": "text-translator2.p.rapidapi.com",
-          },
-          data: encodedParams,
-        };
-
-        try {
-          const response = await axios.request(options);
-          console.log(response.data);
-          const translatedText = response.data.data.translatedText;
-          bot.sendMessage(chatId, `Translated Text    ${translatedText}`);
-        } catch (error) {
-          console.error(error);
-          bot.sendMessage(chatId, "Error translating text");
-        }
+      bot.once("message", (msg) => {
+        translateText(bot, chatId, languageId, msg.text);
       });
     } else {
-      bot.sendMessage(msg.chat.id, "Select the language", {
-        reply_markup: {
-          keyboard: languageOptions,
-        },
-      });
+      sendLanguageSelectionMessage(bot, msg.chat.id);
     }
   });
 };
